6c6ee88bd5d0b19e9afe3ff34d1d2377b6d0e134
Raghav Agarwal <raghav.ag13@gmail.com>
2021-10-06 03:52:36 +0530

    created background.js

---
+++ chrome/background.js
@@ -0,0 +1,11 @@
+// contest-object = {  name(string), startTimeSeconds(int), durationSeconds(int) }
+// allContests = [array of contest-object]
+var allContests = [];
+
+// Issue #9: Use codeforces api to get the list of all upcoming contests (and store it in allContests)
+// @return nothing
+function updateContests() {}
+
+// Issue #9: create a function that will give the nearest upcoming contest
+// @return a contest-object
+function nextContest() {}
