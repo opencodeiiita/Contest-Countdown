0e3415a3837fac3f7653c94f5c5f1bab98622f42
jalanprakhar <iit2020200@iiita.ac.in>
2021-10-07 15:39:53 +0530

    test

--- chrome/scripts.js
+++ chrome/scripts.js
@@ -1,3 +1,4 @@
+
 function remainingTime(targetDate) {
     var currentDate = new Date().getTime();
     var interval = targetDate - currentDate;
@@ -34,6 +35,62 @@ function runClock(targetDate) {
 var timeinterval = setInterval(runClock, 1000);
 
 // Issue #10: Write html and javascript to display contests from the paramter array
-// @params allContests: array of contest-obeject (refer to backgroung.js)
+// @params allContests: array of contest-obeject (refer to backgroung.js)/
+
 // @return none
-function displayContests() {}
+function displayContests(allContests) {
+    const container = document.getElementById("container");
+    // console.log("hello");
+    // console.log("hello");
+    allContests.map((contest) => {
+        let element = document.createElement("div");
+        element.className = "contestDiv";
+        const nametext = document.createElement("p");
+        nametext.innerHTML = `Contest name:- ${contest.Name}`;
+        nametext.className = "contestName";
+
+        const platform = document.createElement("p");
+        platform.innerHTML = `Platform:- ${contest.Platform}`;
+        platform.className = "contestPlatform";
+        // console.log(contest.name);
+
+        const startime = document.createElement("p");
+        startime.innerHTML = `Start Time:- ${contest.StartTime}`;
+        startime.className = "startime";
+
+        const endtime = document.createElement("p");
+        endtime.innerHTML = `End Time:- ${contest.EndTime}`;
+        endtime.className = "endtime";
+
+        const duration = document.createElement("p");
+        duration.innerHTML = `Duration :- ${contest.Duration}`;
+        duration.className = "duration";
+        
+        const link = document.createElement("a");
+        link.setAttribute('href') = `${contest.url}`;
+        link.innerHTML = `Contest link :- ${contest.url}`;
+        link.className = "link";
+
+
+        const line = document.createElement("hr");
+        element.appendChild(nametext);
+        element.appendChild(platform);
+        element.appendChild(startDate);
+        element.appendChild(startime);
+        element.appendChild(duration);
+        element.appendChild(link);
+        element.appendChild(line);
+        container.appendChild(element);
+    });
+}
+displayContests([{
+        
+        "Name": "Data Story Telling", 
+        Platform: "HACKEREARTH",  
+       
+        StartTime: "Fri, 15 Oct 2021 18:00", 
+        EndTime: "Sun, 24 Oct 2021 23:55", 
+        Duration: "9 days 5h 55m", 
+        challenge_type: "contest", 
+        url: "https://www.hackerearth.com/challenges/hiring/data-story-telling/"
+      }]);
